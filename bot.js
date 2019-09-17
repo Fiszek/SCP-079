10ab3c639092f864479d9d761b592a5831203ee2
Fiszek <55458850+Fiszek@users.noreply.github.com>
2019-09-17 20:15:24 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+bot.on('ready', () =>{
+  console.log('Bot jest online!');
+})
+
+bot.login(token);
+
+client.login(process.env.NjIzNTc4MzEyMTM2MTk2MTI2.XYEe3Q.TarYcbqj7VrOuRnVyXK_OzLzluI);
